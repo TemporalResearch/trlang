f5ef95a62e33598c80f9cdf731144f36911d7f5c
Michael Lynch <lyncmi07@yahoo.co.uk>
2021-04-23 23:58:46 +0100

    [Michael] #4 convert tr_lang to use cmake package format

--- libtr_lang-config.cmake
+++ libtr_lang-config.cmake
@@ -1,8 +1,16 @@
 if (TARGET libtr_lang::libtr_lang)
+    message("Target already exists")
     return()
 endif()
 
 add_library(libtr_lang::libtr_lang INTERFACE IMPORTED)
+
+add_subdirectory(${CMAKE_CURRENT_LIST_DIR}/include libtr_lang)
+
+SET_TARGET_PROPERTIES(libtr_lang::libtr_lang PROPERTIES LINKED_LANGUAGE CXX)
+
 target_include_directories(libtr_lang::libtr_lang
         INTERFACE ${CMAKE_CURRENT_LIST_DIR}/include)
+target_link_libraries(libtr_lang::libtr_lang
+        INTERFACE trlang_src_module)
 
